65c7efe90263ac6919fe971520c7ff7f98678b22
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2022-05-24 03:50:22 -0600

    WIP: build out base class process method

--- src/ingest/GraphTicker.ts
+++ src/ingest/GraphTicker.ts
@@ -1,9 +1,10 @@
-import { createApolloFetch, ApolloFetch } from "apollo-fetch";
+import {createApolloFetch, ApolloFetch} from "apollo-fetch";
 
 export default abstract class GraphTicker {
-    ethereumFetchClient: ApolloFetch;
     ethereumBlockHeightQuery: string;
     exchangeName: string;
+    ethereumFetchClient: ApolloFetch;
+    subgraphClient: ApolloFetch;
 
     constructor() {
         this.ethereumFetchClient = createApolloFetch({
@@ -19,6 +20,9 @@ export default abstract class GraphTicker {
               timestamp
             }
         }`
+        this.subgraphClient = createApolloFetch({
+            uri: this.getDEXSubGraphUri()
+        })
     }
 
     async start() {
@@ -31,7 +35,26 @@ export default abstract class GraphTicker {
         console.log('timestamp24HrsAgo:', timestamp24HrsAgo)
         const blockHeight24HoursAgo = await this.getBlockHeight24HrsAgo(timestamp24HrsAgo)
         console.log('blockHeight24HoursAgo', blockHeight24HoursAgo)
+
+        /* The structure of this map is '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': {
+         *   currentData: [{ allTimeVolumeNow: 32342815512.84094, last: 0.000301056878224 }]
+         *   volumes24hrsAgo: ['30342815512.84094', '25529248474.688953']
+         *   pair: {
+         *    base: 'USDC',
+         *    quote: 'ETH'
+         *   }
+         */
+        const pairDataMap = {}
+
+        // The structure of this map is 'USDC/ETH': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+        // aka `${baseSymbol}/${quoteSymbol}`: `${baseId}/${quoteId}`
+        const exchangeMarketIdToContractIdsMap = {}
+
         // TODO: find volumes at current height and blockHeight24HoursAgo
+    }
+
+    getDEXSubGraphUri() {
+        return 'must_be_defined'
     }
 
     getTimestamp24HrsAgo() {
@@ -41,7 +64,7 @@ export default abstract class GraphTicker {
 
     async getBlockHeight24HrsAgo(timestamp24HrsAgo) {
         const {
-            data: { blocks }
+            data: {blocks}
         } = await this.ethereumFetchClient({
             query: this.ethereumBlockHeightQuery,
             variables: {
